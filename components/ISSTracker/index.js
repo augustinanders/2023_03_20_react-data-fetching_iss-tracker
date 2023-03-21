d61586b10029b77905fdc6e7aaf4bdf63c501bf9
Augustin Anders <augustinanders@gmail.com>
2023-03-21 08:47:24 +0100

    solution

--- components/ISSTracker/index.js
+++ components/ISSTracker/index.js
@@ -1,16 +1,32 @@
 import { useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
+import useSWR from "swr";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+const fetcher = async (url) => {
+  const res = await fetch(url);
+
+  // If the status code is not in the range 200-299,
+  // we still try to parse and throw it.
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    // Attach extra info to the error object.
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
 
 export default function ISSTracker() {
-  const [coords, setCoords] = useState({
+  /*   const [coords, setCoords] = useState({
     longitude: 0,
     latitude: 0,
-  });
+  }); */
 
-  async function getISSCoords() {
+  /*   async function getISSCoords() {
     try {
       const response = await fetch(URL);
       if (response.ok) {
@@ -18,11 +34,11 @@ export default function ISSTracker() {
         setCoords({ longitude: data.longitude, latitude: data.latitude });
       }
     } catch (error) {
-      console.error(error);
+      ;
     }
-  }
+  } */
 
-  useEffect(() => {
+  /*   useEffect(() => {
     const timer = setInterval(() => {
       getISSCoords();
     }, 5000);
@@ -30,15 +46,29 @@ export default function ISSTracker() {
     return () => {
       clearInterval(timer);
     };
-  }, []);
+  }, []); */
+
+  const { isLoading, data, mutate, error } = useSWR(URL, fetcher, {
+    refreshInterval: 5000,
+  });
+  console.log(data);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error.info.error}</div>;
+  }
 
   return (
     <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Map longitude={data.longitude} latitude={data.latitude} />
       <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
-        onRefresh={getISSCoords}
+        longitude={data.longitude}
+        latitude={data.latitude}
+        onRefresh={() => {
+          mutate();
+        }}
       />
     </main>
   );
